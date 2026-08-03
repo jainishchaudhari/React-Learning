bfcd31dc72ada873fa0ccbedde90f2b463ae1c7a
Jainish <chaudharijainish9498@gmail.com>
2026-08-04 02:17:31 +0530

    React Hooks: useState()

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+
+let [counter, setCounter] = useState(0)
+
+  // let counter = 5
+
+  const addValue = () => {
+    // counter = counter + 1    
+    setCounter(counter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+      <h1>React</h1>
+      <h2>counter value: {counter}</h2>
+      <button
+      onClick={addValue}
+      >Add value {counter}</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >Remove value {counter}</button>
+    </>
+  );
+}
+
+export default App;
