6c8814afc8718cc8872f6b394e7f1a58d960670f
Jainish <chaudharijainish9498@gmail.com>
2026-08-07 23:38:57 +0530

    password genrator : useState and new useCallback

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,15 +1,62 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useCallback } from "react";
 
 function App() {
-  const [lenght, setLenght] = useState(0)
-  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [symbolsAllowed, setSymbolsAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (numberAllowed) str += "0123456789";
+    if (symbolsAllowed) str += "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, symbolsAllowed, setPassword]);
 
   return (
     <>
-      <h1 className="text-4xl text-center" >password genretor</h1>
+      <div className="w-full max-w-md mx-auto shadow-2xl shadow-cyan-500/30 rounded-xl px-6 py-5 my-8 text-orange-400 bg-gray-700">
+        <h1 className="text-white text-center m-2">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="w-full bg-white text-black outline-none py-2 px-3"
+            placeholder="password"
+            readOnly
+          />
+
+          <button className="outline-none bg-blue-500 text-white px-3 py-0.5 shrink-0">
+            copy
+          </button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input 
+            type="range"
+            min={5}
+            max={20}
+            value={length}
+            className="cursor-pointer"
+            onChange={(e) => {setLength(e.target.value)}}
+            />
+
+            <label>Length: {length}</label>
+
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
