056938e295af6b947e4ce84283e4bc3791cccf4b
Jainish <chaudharijainish9498@gmail.com>
2026-08-14 00:28:09 +0530

    add dark mode theme switcher

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./contexts/Theme";
+import Card from "./components/Card";
+import ThemeBtn from "./components/ThemeBtn";
 
 function App() {
 
@@ -9,20 +11,30 @@ function App() {
   const lightTheme = () => {
     setThemeMode("light")
   }
-  
+
   const darkTheme = () => {
     setThemeMode("dark")
   }
+
+  //actual change in theme
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+  
 
   return (
     <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-            {/* themBtn */}
+            <ThemeBtn />
           </div>
 
-          <div className="w-full max-w-sm mx-auto">{/* card */}</div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>
